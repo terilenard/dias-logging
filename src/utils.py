125dacfcf8a05583babd85a92fcaeb9f4de81559
Teri Lenard <teri.lenard@umfst.ro>
2021-12-27 17:18:05 +0200

    Removed pipe related functions and imports from utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,7 @@
 import os
-import errno
 
 from binascii import hexlify
-from stat import S_IFIFO, S_IRUSR, S_IWUSR
 
-from DiasLogging.logging.handlers import FIFO_FILE
 
 def exists(dir):
     """
@@ -12,38 +9,6 @@ def exists(dir):
     """
     return os.path.exists(dir)
 
-def make_pipe(fifo):
-    # Set up communication structures
-
-    if exists(fifo):
-        return True
-    
-    try:
-        os.mkfifo(fifo, S_IFIFO | S_IRUSR | S_IWUSR)
-        return True
-    except OSError as ex:
-        if (ex.errno != errno.EEXIST):
-            print("Unable to create fifo file\n")
-            print(str(ex))
-            return False
-
-def open_pipe(fifo):
-    return os.open(fifo, os.O_RDONLY)
-
-def close_pipe(fifo):
-    os.close(fifo)
-
-def read_pipe(fifo):
-    print("Opening FIFO...")
-    with open(fifo) as _fifo:
-        print("FIFO opened")
-        while True:
-            data = _fifo.read()
-            if len(data) == 0:
-                print("Writer closed")
-                break
-            print('Read: "{0}"'.format(data))
-            return data
 
 def dump(data, file):
 
