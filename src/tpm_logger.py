7fa6c87a281fc1c031ec9194748be1eede4c4881
Teri Lenard <teri.lenard@umfst.ro>
2022-01-08 10:51:09 +0200

    Changed protobuf communication to pipe

--- src/tpm_logger.py
+++ src/tpm_logger.py
@@ -15,8 +15,8 @@ sys.path.append("/home/teri/Workspace/dias-hackathon-testbed1/modules/communicat
 from utils import *
 from wrapper import TPM2_FlushContext, TPM2_LoadKey, TPM2_Sign, TPM2_Hash, TPM2_ExtendPcr
 
-from comm_core.communicator import Communicator
-from comm_core.proto.logging_pb2 import LogMessage
+#from comm_core.communicator import Communicator
+#from comm_core.proto.logging_pb2 import LogMessage
 
 """
 Dependencies
@@ -54,10 +54,12 @@ class TPMLogger():
         self._app_logger = setup_logger("ServiceTPMLogger", config["log"]["info_log"])
 
         self._tpm_conf = dict(config["tpm"])
-        self._zmq_conf = dict(config["zmq"])
+#        self._zmq_conf = dict(config["zmq"])
         self._key_loaded = False
-        self._communicator = None
+        # self._communicator = None
 
+        self._pipe = config["log"]["fifo"]
+        self._pipe_fd = None
         self._queue = list()
         self._loop = asyncio.get_event_loop()
 
@@ -72,6 +74,19 @@ class TPMLogger():
 
     def _hash(self, msg):
         return sha1(msg.encode()).hexdigest()
+
+    async def _listen_on_pipe(self):
+
+        msg = read_pipe(self._pipe)
+        self._queue.append(msg)
+        self._app_logger.info("Added in queue. New size: {}".format(len(self._queue)))
+
+        if len(self._queue) == 1:
+            logs = [self._queue.pop(0) for i in range(1)]
+            asyncio.run_coroutine_threadsafe(self._sign(logs), self._loop)
+            return
+
+        asyncio.run_coroutine_threadsafe(self._listen_on_pipe(), self._loop)
 
     async def _sign(self, logs):
 
@@ -118,7 +133,13 @@ class TPMLogger():
         self._sec_logger.info(json.dumps(json_log))
         self._app_logger.info("Finished signing")
 
+        asyncio.run_coroutine_threadsafe(self._listen_on_pipe(), self._loop)
+
     def start(self):
+
+        if not make_pipe(self._pipe):
+            # self._info_log.error("Couldn't create fifo.")
+            return False
 
         if not self._check_provision():
             self._app_logger.error("Provision error.")
@@ -136,24 +157,27 @@ class TPMLogger():
         if self._loop is None:
             self._loop = asyncio.get_event_loop()
 
-        self._app_logger.debug("Starting Communicator on {}".format(self._zmq_conf["request_address"]))
+        self._loop.create_task(self._listen_on_pipe())
 
-        self._communicator = Communicator(
-            self._zmq_conf["request_address"],
-            self._on_request,
-            None, # pub address
-            [(self._zmq_conf["requestor_name"], self._zmq_conf["request_address"])],
-            [])
-        self._app_logger.debug("Communicator started.")
+       # self._app_logger.debug("Starting Communicator on {}".format(self._zmq_conf["request_address"]))
+
+        # self._communicator = Communicator(
+        #     self._zmq_conf["request_address"],
+        #     self._on_request,
+        #     None, # pub address
+        #     [(self._zmq_conf["requestor_name"], self._zmq_conf["request_address"])],
+        #     [])
+        # self._app_logger.debug("Communicator started.")
 
         self._app_logger.debug("Starting the loop")
         self._loop.run_forever()
 
     def stop(self):
 
-        if self._communicator:
-            self._communicator.stop()
+        # if self._communicator:
+        #     self._communicator.stop()
 
+        read_event.clear()
         if self._loop.is_running:
             self._app_logger.debug("Stopping the loop...")
 
@@ -166,21 +190,22 @@ class TPMLogger():
             TPM2_FlushContext()
             self._app_logger.debug("Handlers removed from TPM.")
 
-    def _on_request(self, request):
-        
-        log_request = LogMessage()
-        log_request.ParseFromString(request.data)
 
-        if (log_request):
-            request.reply("ok".encode())
-
-        self._queue.append(log_request.message)
-        self._app_logger.info("Added in queue. New size: {}".format(len(self._queue)))
-
-        if len(self._queue) == TPMLogger.MAX_MSGS:
-            logs = [self._queue.pop(0) for i in range(TPMLogger.MAX_MSGS)]
-            asyncio.run_coroutine_threadsafe(self._sign(logs), self._loop)
-            return
+    # def _on_request(self, request):
+    #
+    #     # log_request = LogMessage()
+    #     # log_request.ParseFromString(request.data)
+    #
+    #     if (log_request):
+    #         request.reply("ok".encode())
+    #
+    #     self._queue.append(log_request.message)
+    #     self._app_logger.info("Added in queue. New size: {}".format(len(self._queue)))
+    #
+    #     if len(self._queue) == TPMLogger.MAX_MSGS:
+    #         logs = [self._queue.pop(0) for i in range(TPMLogger.MAX_MSGS)]
+    #         asyncio.run_coroutine_threadsafe(self._sign(logs), self._loop)
+    #         return
   
 
 def signal_handler(signum, frame):
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
 import os
+import errno
 
 from binascii import hexlify
+from stat import S_IFIFO, S_IRUSR, S_IWUSR
+from threading import Event
 
+read_event = Event()
 
 def exists(dir):
     """
@@ -10,16 +14,54 @@ def exists(dir):
     return os.path.exists(dir)
 
 
-def dump(data, file):
+def make_pipe(fifo):
+    # Set up communication structures
 
-    with open(file, 'w', encoding="utf-8") as f:
+    if exists(fifo):
+        return True
+
+    try:
+        os.mkfifo(fifo, S_IFIFO | S_IRUSR | S_IWUSR)
+        return True
+    except OSError as ex:
+        if (ex.errno != errno.EEXIST):
+            print("Unable to create fifo file\n")
+            print(str(ex))
+            return False
+
+
+def open_pipe(fifo):
+    return os.open(fifo, os.O_RDONLY)
+
+
+def close_pipe(fifo):
+    os.close(fifo)
+
+
+def read_pipe(fifo):
+    print("Opening FIFO...")
+    with open(fifo) as _fifo:
+        print("FIFO opened")
+        read_event.set()
+        while read_event.is_set():
+            data = _fifo.read()
+            if len(data) == 0:
+                print("Writer closed")
+                break
+            print('Read: "{0}"'.format(data))
+            return data
+
+
+def dump(data, file):
+    with open(file, 'w') as f:
         for line in data:
             f.write(str(line))
+
 
 def load_binary(file):
     data = ''
 
     with open(file, 'rb') as f:
         data = f.read()
-    
+
     return hexlify(data).decode()
