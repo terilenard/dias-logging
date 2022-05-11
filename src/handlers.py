9e0cfdc1a457d2eed3c7f1d45e90af63e251e312
Teri Lenard <teri.lenard@umfst.ro>
2022-05-11 15:33:51 +0300

    Added license and contributors in source files

--- src/handlers.py
+++ src/handlers.py
@@ -1,3 +1,13 @@
+"""
+This work is licensed under the terms of the MIT license.  
+For a copy, see <https://opensource.org/licenses/MIT>.
+
+Developed by NISLAB - Network and Information Security Laboratory
+at George Emil Palade University of Medicine, Pharmacy, Science and
+Technology of Târgu Mureş <https://nislab.umfst.ro/>
+
+Contributors: Teri Lenard
+"""
 
 TPM2_PRIMARY_CTX = "/home/pi/workspace/dias-logging/Src/DiasLogging/Bin/primary.ctx"
 TPM2_PRIMARY_HNDLR = "0x81010002"
